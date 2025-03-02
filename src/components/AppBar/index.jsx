0ce7e29c7c6726c233092b1b11680ff18cd10d27
thanhleesenpai <thanhle20072004@gmail.com>
2025-03-02 16:28:13 +0700

    relative absolute import

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,5 +1,5 @@
 import Box from '@mui/material/Box'
-import ModeSelect from '../../components/ModeSelect'
+import ModeSelect from '~/components/ModeSelect'
 
 function AppBar() {
   return (
@@ -11,7 +11,7 @@ function AppBar() {
       alignItems: 'center'
     }}>
       <ModeSelect />
-    </Box>  
+    </Box>
   )
 }
 
