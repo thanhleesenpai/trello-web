34639a4c18239b7d16daad06e032981b2bc4fd38
thanhleesenpai <thanhle20072004@gmail.com>
2025-03-04 21:55:50 +0700

    design board bar

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -17,6 +17,7 @@ import Badge from '@mui/material/Badge'
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
 import Tooltip from '@mui/material/Tooltip'
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
+import LibraryAddIcon from '@mui/icons-material/LibraryAdd'
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -61,13 +62,14 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 }))
 function AppBar() {
   return (
-    <Box px={ 2 } sx={{
+    <Box sx={{
       width:'100%',
       height: (theme) => theme.trello.appBarHeight,
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'space-between',
       gap: 2,
+      paddingX: 2,
       overflowX: 'auto'
     }}>
       <Box sx= {{ display: 'flex', alignItems: 'center', gap: 2 }}>
@@ -84,7 +86,7 @@ function AppBar() {
           <Recent />
           <Starred />
           <Templates />
-          <Button variant="outlined">Create</Button>
+          <Button variant="outlined" startIcon={<LibraryAddIcon />}>Create</Button>
         </Box>
 
       </Box>
