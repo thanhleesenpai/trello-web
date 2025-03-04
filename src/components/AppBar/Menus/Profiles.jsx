638a83c681df6e4a82b5e785b37e9166ed8feab8
thanhleesenpai <thanhle20072004@gmail.com>
2025-03-05 00:00:10 +0700

    refactor theme color

--- src/components/AppBar/Menus/Profiles.jsx
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -32,7 +32,7 @@ function Profiles() {
           aria-expanded={open ? 'true' : undefined}
         >
           <Avatar
-            sx={{ width: 34, height: 34 }}
+            sx={{ width: 36, height: 36 }}
             alt="Thanhle"
             src="https://scontent.fhan14-5.fna.fbcdn.net/v/t39.30808-1/291684198_614607523335861_3331449266675353218_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=109&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeFFXKxwrftNs8Au6Znq-O5hAgDfZjkmLfECAN9mOSYt8cFiHbAxRg8FDgtcV2eXHg7d2wQ7-jf3OeLMwPlWb15Z&_nc_ohc=a0t9OoAOXE4Q7kNvgH32hVp&_nc_oc=AdiLaaNPwLO_RQdGfKWKaesvX-noN366C44HXvjuzZ-pDwp9DW-8kKY7Ml-Zvbu5NPO2-xodRQu2s8Nt3QhafIcU&_nc_zt=24&_nc_ht=scontent.fhan14-5.fna&_nc_gid=Au2_8cKZvClmXxoxSIut7T2&oh=00_AYBtWDqbbKUtKCjIvZ1uL3PTa4Hsn9YI3N5BPA2La230dQ&oe=67CA096E"
           />
--- src/theme.js
+++ src/theme.js
@@ -9,18 +9,18 @@ const theme = extendTheme({
     boardBarHeight: '58px'
   },
   colorSchemes: {
-    light: {
-      palette: {
-        primary: teal,
-        secondary: deepOrange
-      }
-    },
-    dark: {
-      palette: {
-        primary: cyan,
-        secondary: orange
-      }
-    }
+    // light: {
+    //   palette: {
+    //     primary: teal,
+    //     secondary: deepOrange
+    //   }
+    // },
+    // dark: {
+    //   palette: {
+    //     primary: cyan,
+    //     secondary: orange
+    //   }
+    // }
   },
   components: {
     MuiCssBaseline: {
@@ -31,11 +31,11 @@ const theme = extendTheme({
             height: '8px'
           },
           '*::-webkit-scrollbar-thumb ': {
-            backgroundColor: '#bdc3c7',
+            backgroundColor: '#dcdde1',
             borderRadius: '8px'
           },
           '*::-webkit-scrollbar-thumb:hover': {
-            backgroundColor: '#7f8c8d'
+            backgroundColor: 'white'
           }
         }
       }
@@ -43,35 +43,27 @@ const theme = extendTheme({
     MuiButton: {
       styleOverrides: {
         root: {
-          textTransform: 'none'
+          textTransform: 'none',
+          borderWidth: '0.5px',
+          '&:hover': { borderWidth: '0.5px' }
         }
       }
     },
     MuiInputLabel: {
       styleOverrides: {
-        root: ({ theme }) => ({
-          color: theme.palette.primary.main,
+        root: {
           fontSize:'0.875rem'
-        })
+        }
       }
     },
     MuiOutlinedInput: {
       styleOverrides: {
-        root: ({ theme }) => ({
-          color: theme.palette.primary.main,
+        root: {
           fontSize:'0.875rem',
-          '.MuiOutlinedInput-notchedOutline': {
-            borderColor: theme.palette.primary.light
-          },
-          '&:hover': {
-            '.MuiOutlinedInput-notchedOutline': {
-              borderColor: theme.palette.primary.main
-            }
-          },
-          '& fieldset': {
-            borderWidth: '1px !important'
-          }
-        })
+          '& fieldset': { borderWidth: '0.5px !important' },
+          '&:hover fieldset': { borderWidth: '1px !important' },
+          '&.Mui-focused fieldset': { borderWidth: '1px !important' }
+        }
       }
     }
   }
