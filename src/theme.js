7e22d08621d564c80abdcd389696d614e738cba9
thanhleesenpai <thanhle20072004@gmail.com>
2025-03-04 16:33:16 +0700

    design app bar part 3

--- src/theme.js
+++ src/theme.js
@@ -23,6 +23,23 @@ const theme = extendTheme({
     }
   },
   components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          '*::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px'
+          },
+          '*::-webkit-scrollbar-thumb ': {
+            backgroundColor: '#bdc3c7',
+            borderRadius: '8px'
+          },
+          '*::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#7f8c8d'
+          }
+        }
+      }
+    },
     MuiButton: {
       styleOverrides: {
         root: {
