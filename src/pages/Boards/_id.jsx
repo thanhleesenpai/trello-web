80c02a44057e63fd0c212ac03ee1c373ac877431
thanhleesenpai <thanhle20072004@gmail.com>
2025-03-20 17:06:57 +0700

    react toastify

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,7 +3,7 @@ import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import { useEffect, useState } from 'react'
-import { mockData } from '~/apis/mooc-data'
+//import { mockData } from '~/apis/mooc-data'
 import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
@@ -20,8 +20,8 @@ function Board() {
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData.board} />
-      <BoardContent board={mockData.board} />
+      <BoardBar board={board} />
+      <BoardContent board={board} />
     </Container>
   )
 }
